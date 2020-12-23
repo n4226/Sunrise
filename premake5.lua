e4525886eb088f5c819da81b3c514480a983953e
Mike <testtset638@gmail.com>
2020-12-23 08:35:02 -0800

    add premake

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+
+
+project "Sunrise"
+
+	kind "SharedLib"
+	langauge "C++"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+
+	files {
+		"src/**.h",
+		"src/**.cpp",
+
+		
+	}
+
+	includedirs {
+		"src"
+	}	
+
+	filter "system:windows"
+		
+		cppdialect "C++17"
+		staticruntime "on"
+
+		defines {
+			"SR_PLATFORM_WINDOWS",
+			"SR_BUILD_DLL"
+		}
+
+
+	filter "configurations:Debug"
+		defines "SR_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "SR_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configrations:Dist"
+		defines "SR_DIST"
+		runtime "Release"
+		optimize "on"
