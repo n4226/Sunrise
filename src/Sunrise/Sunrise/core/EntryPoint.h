e73185680413d8800b676c2fe991031a84f3d139
Mike <testtset638@gmail.com>
2021-06-24 19:56:17 -0400

    made entry point optional

--- src/Sunrise/Sunrise/core/EntryPoint.h
+++ src/Sunrise/Sunrise/core/EntryPoint.h
@@ -8,17 +8,28 @@
 
 extern sunrise::Application* sunrise::CreateApplication();
 
+namespace sunrise {
+
+	void runEngine() {
+		auto app = sunrise::CreateApplication();
+
+		sunrise::engine = new sunrise::Engine(app);
+
+		sunrise::engine->startup();
+
+		delete sunrise::engine;
+	}
+
+}
+
+// for programs such as dlls which are not in control of their main function allow them to call runEngine on their own
+#ifndef SR_NO_AUTO_ENTRY
 int main(int arc, char** argv) {
 
-	auto app = sunrise::CreateApplication();
-
-	sunrise::engine = new sunrise::Engine(app);
-
-	sunrise::engine->startup();
-
-	delete sunrise::engine;
+	sunrise::runEngine();
 
 	return 0;
 }
+#endif
 
 #endif 
